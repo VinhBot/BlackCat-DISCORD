47a11c9e8c1c084606376f6d3fe8f07589a6d9ef
VinhBot <115027531+VinhBot@users.noreply.github.com>
2024-01-20 01:47:25 +0700

    sửa lại

--- Handlers/messageCreate.js
+++ Handlers/messageCreate.js
@@ -1,19 +1,6 @@
-import fs from "node:fs/promises";
-import nodeUrl from "node:url";
-
-function globalFilePath(path) {
-    return nodeUrl.pathToFileURL(path)?.href || path;
-};
-
-export default async function (client) {
-    const commandFiles = await fs.readdir(globalFilePath("./Commands/PrefixCommands/Test"));
-    for (const file of commandFiles) {
-        if (file.endsWith('.js')) {
-            const command = await import(globalFilePath(`./Commands/PrefixCommands/Test/${file}`)).then((e) => e.default);
-            client.commands.set(command.name, command);
-        };
-    };
-
+export default async function messageCreate(client) {
+    const command = await import(`../Commands/PrefixCommands/Test/ping.js`).then((e) => e.default);
+    client.commands.set(command.name, command);
     client.on('messageCreate', (message) => {
         if (message.author.bot || !message.content.startsWith(client.config.prefix)) return;
         const args = message.content.slice(client.config.prefix.length).trim().split(/ +/);
